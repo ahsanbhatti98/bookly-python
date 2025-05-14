9a5ba902f07de5475047a9103d2084585a760174
ahsanbhatti98 <ahsanbhatti98@gmail.com>
2025-05-14 18:41:49 +0500

    Implement Book model, service, and routes with CRUD operations; add database session management

--- src/db/main.py
+++ src/db/main.py
@@ -1,18 +1,22 @@
-from sqlmodel import create_engine, text
 from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.orm import sessionmaker
+from sqlmodel import SQLModel, create_engine
+from sqlmodel.ext.asyncio.session import AsyncSession
+
 from src.config import Config
 
+async_engine = AsyncEngine(create_engine(url=Config.DATABASE_URL))
 
-engine = AsyncEngine(
-    create_engine(
-        url=Config.DATABASE_URL,
-        echo=True,
+
+async def init_db() -> None:
+    async with async_engine.begin() as conn:
+        await conn.run_sync(SQLModel.metadata.create_all)
+
+
+async def get_session() -> AsyncSession:
+    Session = sessionmaker(
+        bind=async_engine, class_=AsyncSession, expire_on_commit=False
     )
-)
 
-
-async def init_db():
-    async with engine.begin() as conn:
-        statement = text("SELECT 'hello';")
-        result = await conn.execute(statement)
-        print(result.all())
+    async with Session() as session:
+        yield session
